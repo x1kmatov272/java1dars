1a6ea4eb9618e8606ad4b5791cf2be8ce5778492
x1kmatov272 <kamronbekxikmatov50@gmail.com>
2025-03-22 09:14:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let info = prompt('Укажите имя ')
+ console.log(info)
+
+ let age = prompt('Ведите свой возраст')
+ console.log(age)
+
+ let arr = prompt('Кде вы живёте')
+ console.log(arr)
+
+ let aff = prompt('Yoqatirgan Faningizni yozing')
+ console.log(aff)
+
+ let dee = prompt('Yashaydigan tumaningizni kritng')
+ console.log(dee)
+
+ alert(`Меня зовут ${info }`)
